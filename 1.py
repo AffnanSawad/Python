804e6445641aedba33374f033ea2ae5db715c487
Affnan Sawad <affnansawad2002@gmail.com>
2025-09-01 20:02:34 +0600

    Chapter 1 Completed

--- 1.py
+++ 1.py
@@ -1,2 +1,15 @@
+# How to print in Python
+print("Hello World !") 
 
- Modules, Comments & pip
+# install: pip install pyjokes
+import pyjokes
+
+create_joke = pyjokes.get_joke()
+print(create_joke)
+
+# Multiline prints using triple quotes
+print("""This is a multiline print,
+      using triple quotes.
+      It can span multiple lines.""")
+
+
